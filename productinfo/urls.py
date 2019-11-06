f90aaafb2647753a396f5742c2ecbcbf0f123d88
neethu481 <neethooz14@gmail.com>
2019-11-06 00:52:13 -0500

    my file new file again

--- productinfo/urls.py
+++ productinfo/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
-from .views import productinfo
-urlpatterns=[
-
-    path('productinfo',productinfo)
+from .views import productinfo, productlist
 
 
+urlpatterns = [
+    path ('productinfo', productinfo ),
+    path ('productlist', productlist ),
 ]
--- productinfo/views.py
+++ productinfo/views.py
@@ -1,14 +1,50 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+
+from django.http import  HttpResponse
 from django.template import loader
+from .models import Product
+
+
 
 def productinfo(req):
+
+    vals =req.GET
+    pid= vals['id']
+    print (pid)
+
     template=loader.get_template("productinfo.html")
-    data={"name":"Vivo",
-          "desc":"Smartphone",
-          "price":455555
-          }
+
+    product = Product.objects.get(id=pid)
+    data= {"product":product}
+    #data={}
+    response= HttpResponse(template.render(data,req))
+
+    cstr= ""
+    print (req.COOKIES)
+    if "INT_PROD" in req.COOKIES:
+        old_ids = req.COOKIES['INT_PROD']
+        cstr= old_ids+ "|" + str ( product.id)
+    else:
+        cstr=product.id
+
+    response.set_cookie("INT_PROD", cstr )
+
+    return response
+
+
+
+def productlist(req):
+    template=loader.get_template("productlist.html")
+
+    plist = Product.objects.all()
+
+    data= {
+
+        "products": plist
+
+    }
     return HttpResponse(template.render(data,req))
+
 
